97d1c921ee87025b6ad86048469d491ed34be923
zerakjamil <zerakjamil987@gmail.com>
2025-04-02 01:10:56 +0300

    add localization support; implement Kurdish translations for app and sales components

---
+++ resources/js/i18n.ts
@@ -0,0 +1,25 @@
+import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'kur',
+    debug: process.env.NODE_ENV === 'development',
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    detection: {
+      order: ['localStorage', 'cookie', 'navigator'],
+      caches: ['localStorage', 'cookie'],
+    },
+  });
+
+export default i18n;
