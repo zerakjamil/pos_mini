d6594e2301b7f29198e49ddf89a9077cd19a5197
zerakjamil <zerakjamil987@gmail.com>
2025-04-15 21:26:44 +0300

    add localization support for appearance settings; implement translations for breadcrumbs, page title, and headings in Appearance component

--- resources/js/pages/settings/appearance.tsx
+++ resources/js/pages/settings/appearance.tsx
@@ -1,4 +1,5 @@
 import { Head } from '@inertiajs/react';
+import { useTranslation } from 'react-i18next';
 
 import AppearanceTabs from '@/components/appearance-tabs';
 import HeadingSmall from '@/components/heading-small';
@@ -7,21 +8,26 @@ import { type BreadcrumbItem } from '@/types';
 import AppLayout from '@/layouts/app-layout';
 import SettingsLayout from '@/layouts/settings/layout';
 
-const breadcrumbs: BreadcrumbItem[] = [
-    {
-        title: 'Appearance settings',
-        href: '/settings/appearance',
-    },
-];
-
 export default function Appearance() {
+    const { t } = useTranslation('settings/appearance');
+
+    const breadcrumbs: BreadcrumbItem[] = [
+        {
+            title: t('breadcrumb'),
+            href: '/settings/appearance',
+        },
+    ];
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Appearance settings" />
+            <Head title={t('page_title')} />
 
             <SettingsLayout>
                 <div className="space-y-6">
-                    <HeadingSmall title="Appearance settings" description="Update your account's appearance settings" />
+                    <HeadingSmall
+                        title={t('heading.title')}
+                        description={t('heading.description')}
+                    />
                     <AppearanceTabs />
                 </div>
             </SettingsLayout>
