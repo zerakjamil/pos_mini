9883d1e74f122f9b7aea842caff5edb646e6494d
zerakjamil <zerakjamil987@gmail.com>
2025-04-19 14:32:06 +0300

    add about page; implement team and mission sections with developer profiles and contact information

--- public/build/assets/fade-B0zGfLQg.js
+++
@@ -1,4 +0,0 @@
-import{D as e}from"./app-CoXKttN8.js";import{i as r}from"./useVariants-C2zpy4FK.js";const s=new e("antFadeIn",{"0%":{opacity:0},"100%":{opacity:1}}),m=new e("antFadeOut",{"0%":{opacity:1},"100%":{opacity:0}}),f=function(a){let i=arguments.length>1&&arguments[1]!==void 0?arguments[1]:!1;const{antCls:o}=a,n=`${o}-fade`,t=i?"&":"";return[r(n,s,m,a.motionDurationMid,i),{[`
-        ${t}${n}-enter,
-        ${t}${n}-appear
-      `]:{opacity:0,animationTimingFunction:"linear"},[`${t}${n}-leave`]:{animationTimingFunction:"linear"}}]};export{f as i};
